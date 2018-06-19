426bf6d058a9ed0cf52e9b455575bd924e72b588
webosmotic-developer <dev.webosmotic@gmail.com>
2018-06-19 19:07:43 +0530

    implemented line width , color picker

--- src/app/components/whiteboard/whiteboard.component.ts
+++ src/app/components/whiteboard/whiteboard.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output } from '@angular/core';
-import {SocketService} from '../../common/services/socket/socket.service';
-import {EmitterService} from '../../common/services/emitter/emitter.service';
-import {AuthService} from '../../common/services/auth/auth.service';
+import { SocketService } from '../../common/services/socket/socket.service';
+import { EmitterService } from '../../common/services/emitter/emitter.service';
+import { AuthService } from '../../common/services/auth/auth.service';
 
 @Component({
   selector: 'app-whiteboard',
@@ -16,6 +16,7 @@ export class WhiteboardComponent implements OnInit, AfterViewInit, OnChanges {
   public drawing: any;
   public current: any;
   public user: any;
+  public lineWidth: any;
   @Input() selectedUserInfo: string;
   @Input() selectedUser: any;
   @Output() EventShowChat: any = new EventEmitter<any>();
@@ -23,6 +24,7 @@ export class WhiteboardComponent implements OnInit, AfterViewInit, OnChanges {
   constructor(private socketService: SocketService, private authService: AuthService) {
     this.user = authService.getAuthUser();
     this.drawing = false;
+    this.lineWidth = 2;
     this.current = {
       'color': 'black',
     };
@@ -36,18 +38,27 @@ export class WhiteboardComponent implements OnInit, AfterViewInit, OnChanges {
     this.socketService.receiveDrawingData().subscribe((data: any) => {
       /* subscribing for messages statrts */
       if (this.selectedUser !== null && this.selectedUser.id === data.fromUserId) {
-       this.onDrawingEvent(data);
+        this.onDrawingEvent(data);
       }
     });
   }
 
+  fnSetlineWidth(size) {
+    this.lineWidth = size;
+  }
 
-  fnDrawLine = (x0, y0, x1, y1, color, emit?: any) => {
+  onColorUpdate = (e) => {
+    this.current.color = e.target.value;
+  }
+
+
+  fnDrawLine = (x0, y0, x1, y1, color, lineWidth, emit?: any) => {
     this.context.beginPath();
     this.context.moveTo(x0, y0);
     this.context.lineTo(x1, y1);
-    this.context.lineWidth = 2;
-    this.context.strokeStyle = 'red';
+    this.context.lineCap = 'round';
+    this.context.lineWidth = lineWidth;
+    this.context.strokeStyle = color;
     this.context.stroke();
     this.context.closePath();
 
@@ -57,52 +68,53 @@ export class WhiteboardComponent implements OnInit, AfterViewInit, OnChanges {
     const w = this.canvas['width'];
     const h = this.canvas['height'];
 
-    this.socketService.drawLine( {
+    this.socketService.drawLine({
       x0: x0 / w,
       y0: y0 / h,
       x1: x1 / w,
       y1: y1 / h,
       color: color,
+      lineWidth: lineWidth,
       fromUserId: this.user.id,
       toUserId: this.selectedUser.id
     });
-  }
+  };
 
   fnOnMouseDown = (e: any) => {
     this.drawing = true;
     this.current.x = e.offsetX;
     this.current.y = e.offsetY;
-  }
+  };
 
   fnOnMouseUp = (e: any) => {
     if (!this.drawing) {
       return;
     }
     this.drawing = false;
-    this.fnDrawLine(this.current.x, this.current.y, e.offsetX, e.offsetY, this.current.color, true);
-  }
+    this.fnDrawLine(this.current.x, this.current.y, e.offsetX, e.offsetY, this.current.color, this.lineWidth, true);
+  };
 
   fnOnMouseMove = (e: any) => {
 
     if (!this.drawing) {
       return;
     }
-    this.fnDrawLine(this.current.x, this.current.y, e.offsetX, e.offsetY, this.current.color, true);
+    this.fnDrawLine(this.current.x, this.current.y, e.offsetX, e.offsetY, this.current.color, this.lineWidth, true);
     this.current.x = e.offsetX;
     this.current.y = e.offsetY;
-  }
+  };
 
   onDrawingEvent = (data) => {
     const w = this.canvas['width'];
     const h = this.canvas['height'];
-    this.fnDrawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-  }
+    this.fnDrawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color, data.lineWidth);
+  };
 
   fnInit() {
     setTimeout(() => {
       this.canvas = document.getElementsByClassName('whiteboard')[0];
-      this.canvas.width = this.canvas.parentNode.clientWidth;
-      this.canvas.height = this.canvas.parentNode.clientHeight;
+      this.canvas.width = this.canvas.parentNode.clientWidth - 10;
+      this.canvas.height = this.canvas.parentNode.clientHeight - 10;
       this.context = this.canvas.getContext('2d');
     }, 0);
   }
