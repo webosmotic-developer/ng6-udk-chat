e76969bb53abd7e099a0a814706e60df0ed8efec
webosmotic-developer <dev.webosmotic@gmail.com>
2018-07-10 11:15:17 +0530

    - Changes for deploying

--- src/app/common/constant.ts
+++ src/app/common/constant.ts
@@ -1,7 +1,7 @@
 export class Constant {
   public static TOKEN_NAME = 'AUTH_TOKEN';
-  public static API_URL = 'http://192.168.0.218:4000/';
-  public static SOCKET_URL = 'http://192.168.0.218:4000/';
+  public static API_URL = 'http://192.168.0.203:4000/';
+  public static SOCKET_URL = 'http://192.168.0.203:4000/';
   public static EMAIL_REG_EX = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$');
   public static PASSWORD_REG_EX = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z!@#$%^&*?.+-\\d]{8,}$');
 }
