5e48ab343d9545a965b825a125beedd2ac7a9599
webosmotic-developer <dev.webosmotic@gmail.com>
2018-07-10 11:59:33 +0530

    - Changes for deploying

--- src/main.server.ts
+++ src/main.server.ts
@@ -42,13 +42,10 @@ class Server {
     this.includeRoutes();
 
     const port = process.env.PORT || 4000;
-    const host = process.env.HOST || '0.0.0.0';
 
     console.log('------------------------*************  port', port);
-    console.log('-------------------------***********host', host);
-
-    this.http.listen(port, host, () => {
-      console.log(`Listening on http://${host}:${port}`);
+    this.http.listen(port, '0.0.0.0', () => {
+      console.log(`Listening on ${port}`);
     });
   }
 
